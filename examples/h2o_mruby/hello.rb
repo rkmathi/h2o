c02314a72834a678d57dbc0a09ffeabc228fb676
Ryuichi KAWAMATA <iam@rkmathi.net>
2016-08-21 19:17:34 +0900

    Naive user authentication (only id)

--- examples/h2o_mruby/hello.rb
+++ examples/h2o_mruby/hello.rb
@@ -1,21 +1,54 @@
-class Hello
+class App
+  RES_HEADER = { 'Content-Type' => 'application/json; charset=utf-8' }
+
+  def self.init_db(host, user, password, table)
+    MySQL::Database.new(host, user, password, table)
+  end
+
+  def initialize
+    @db ||= App.init_db('localhost', 'root', '', 'authmysql')
+    super
+  end
+
+  # @param [Array<String>, String] like ['select * from table where col1=?', 1]
+  # @return [Array<Hash>] like [{col1: val1, col2: val2}, {col1: val3, col2: val4}]
+  def select_db(query)
+    results = []
+    @db.execute(*query) do |row, fields|
+      hash = {}
+      fields.each_with_index { |field, i| hash[field.to_sym] = row[i] }
+      results << hash
+    end
+    results
+  end
+
+  def response(status, msg)
+    [status, RES_HEADER, [msg]]
+  end
+
+  def render(method, path, query, input, x_headers)
+    id = x_headers['HTTP_X_ID'].to_i
+    results = select_db(['select * from users where id=?', id])
+    return response(404, "Not Found\n") if results.empty?
+
+    user = results.first
+    response(200, "id: #{user[:id]}\nname: #{user[:name]}\nuser_group: #{user[:user_group]}")
+
+    #res_msg = "method: #{method}\npath: #{path}\nquery: #{query}\ninput: #{input}\nx_headers: #{x_headers}\n"
+    #response(200, res_msg)
+  end
+
   def call(env)
     req_method = env['REQUEST_METHOD']
     req_path = env['PATH_INFO']
     req_query = env['QUERY_STRING']
     req_input = env['rack.input'] ? env['rack.input'].read : ''
-    [
-      200,
-      { 'Content-Type' => 'application/json; charset=utf-8' },
-      [
-        "req_method: #{req_method}\n",
-        "req_path: #{req_path}\n",
-        "req_query: #{req_query}\n",
-        "req_input: #{req_input}\n"
-      ]
-    ]
+    req_x_headers = env.select { |k, v| k[0, 6] == 'HTTP_X' }
+    render(req_method, req_path, req_query, req_input, req_x_headers)
   end
 end
+
+App.new
 
 ### YAML test
 #puts 'YAML'
@@ -68,6 +101,3 @@ end
 #redis.flushmysql
 #redis['hoge'] = 'hogehoge'
 #p redis['hoge']
-
-
-Hello.new
